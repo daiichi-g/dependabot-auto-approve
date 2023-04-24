0b7c20e7cf3b6ada5a4dce2b1fb4efc177e3b42e
t-fujisaka <t-fujisaka@daiichi-g.com>
2023-04-24 13:46:40 +0900

    パラメータを設定

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token: string = core.getInput('github-token')
+    const isCheckOnly: boolean = core.getInput('check-only') === 'true'
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const pullRequest = github.context.payload.pull_request
+
+    core.debug(`token: ${token.length > 0 ? '*****' : ''}...`)
+    core.debug(`isCheckOnly: ${isCheckOnly}`)
+    core.debug('======================== pullRequest ========================')
+    core.debug(`${JSON.stringify(pullRequest, null, '    ')}`)
+    core.debug('======================== context ========================')
+    core.debug(`${JSON.stringify(github.context, null, '    ')}`)
+    core.debug('=========================================================')
+
+    // アクションの実行イベントを検証
+    if (!pullRequest) {
+      core.setFailed('ERROR: プルリクエストのイベントから実行してください。')
+      return
+    }
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
